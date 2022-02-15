2136023e7e7f39140faac5112526566c9770fb4d
Bohdan <panchyshyn33@gmail.com>
2022-02-16 00:08:26 +0200

    homework1 added

---
+++ homework-1/homework1.js
@@ -0,0 +1,39 @@
+const SSD = 15.678;
+const Memory = 123.965;
+const CPU = 90.2345;
+
+const resultMax = Math.max(SSD, Memory, CPU);
+const resultMin = Math.min(SSD, Memory, CPU);
+
+const resultSumm = SSD + Memory + CPU;
+
+const result = Math.floor(SSD) + Math.floor(Memory) + Math.floor(CPU);
+
+const resultSummSot = Math.ceil((resultSumm)/100)*100;
+
+
+if(result % 2){
+  a = false;
+} else {
+  a = true;
+}
+
+const otherFunds = 500 - resultSumm;
+const averageValue = (resultSumm / 3).toFixed(2);
+const randomNumber = (Math.random()*(40-1)+1).toFixed();
+const discount = resultSumm*(randomNumber/100);
+const discountSumm = (resultSumm - discount).toFixed(2);
+const netProfit = (resultSumm / 2);
+const netProfitResult = (netProfit - discount).toFixed(2);
+
+console.log('Максимальне число: ' + resultMax);
+console.log('Мінімальне число: ' + resultMin);
+console.log('Сума товарів: ' + resultSumm);
+console.log('Сума цілих чисел товарів: ' + result);
+console.log('Сума округлення до сотень : ' + resultSummSot);
+console.log('Сума усіх товарі є парною: ' +  a);
+console.log('Сума решти при оплаті 500грн: ' + otherFunds + ' грн.');
+console.log('Середнє значення цін: ' + averageValue);
+console.log('Ви отримали знижку: ' + randomNumber + '%');
+console.log('Ваша сумма із врахуванням знижки: ' + discountSumm  + ' грн.');
+console.log('Чистий прибуток: ' + netProfitResult);
