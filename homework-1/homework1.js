c86e033f3ca9c74ddc51abf9711ecd333193f0af
Bohdan <panchyshyn33@gmail.com>
2022-02-16 00:10:07 +0200

    homework1 added

--- homework-1/homework1.js
+++ homework-1/homework1.js
@@ -26,14 +26,31 @@ const discountSumm = (resultSumm - discount).toFixed(2);
 const netProfit = (resultSumm / 2);
 const netProfitResult = (netProfit - discount).toFixed(2);
 
-console.log('Максимальне число: ' + resultMax);
+/*console.log('Максимальне число: ' + resultMax);
 console.log('Мінімальне число: ' + resultMin);
 console.log('Сума товарів: ' + resultSumm);
 console.log('Сума цілих чисел товарів: ' + result);
-console.log('Сума округлення до сотень : ' + resultSummSot);
+console.log('Сума округлення до сотень: ' + resultSummSot);
 console.log('Сума усіх товарі є парною: ' +  a);
 console.log('Сума решти при оплаті 500грн: ' + otherFunds + ' грн.');
 console.log('Середнє значення цін: ' + averageValue);
 console.log('Ви отримали знижку: ' + randomNumber + '%');
 console.log('Ваша сумма із врахуванням знижки: ' + discountSumm  + ' грн.');
-console.log('Чистий прибуток: ' + netProfitResult);
+console.log('Чистий прибуток: ' + netProfitResult);*/
+
+alert
+(
+    `
+     Максимальне число: ${resultMax}
+     Мінімальне число: ${resultMin}
+     Сума товарів: ${resultSumm}
+     Сума цілих чисел товарів: ${result}
+     Сума округлення до сотень: ${resultSummSot}
+     Сума усіх товарі є парною: ${a}
+     Сума решти при оплаті 500грн: ${otherFunds} грн.
+     Середнє значення цін: ${averageValue}
+     Ви отримали знижку: ${randomNumber}%
+     Ваша сумма із врахуванням знижки: ${discountSumm} грн.
+     Чистий прибуток: ${netProfitResult}
+     `
+ )
