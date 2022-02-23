0a1d8a9a67dd6a755dd9aa6b59f77935dff191d4
Bohdan <panchyshyn33@gmail.com>
2022-02-24 01:52:33 +0200

    homework2

--- homework2/homework2.js
+++ homework2/homework2.js
@@ -3,28 +3,26 @@ let sumEvenNumbers = 0;
 let startNumber;
 
 do{
-    startNumber = +prompt("Введіть число N від якого будемо складати: ");
-}while(isNaN(startNumber) || startNumber < 0);
+    startNumber = +prompt("Введіть число N від якого будемо складати: ", 0);
+}while(isNaN(startNumber) || startNumber <= 0);
 
 let finalNumber;
 
 do{
-    finalNumber = +prompt("Введіть число M до якого будемо складати: ");
-}while(isNaN(finalNumber) || finalNumber < 0);
+    finalNumber = +prompt("Введіть число M до якого будемо складати: ", 0);
+}while(isNaN(finalNumber) || finalNumber <= 0 || finalNumber < startNumber);
 
 let evenNumbers = confirm("Чи потрібно пропустити парні числа?");
-
-if(evenNumbers){
   
     for(startNumber; startNumber<=finalNumber; startNumber++){
-      if(startNumber % 2){
+
+      if(!evenNumbers){
+
         sumEvenNumbers = sumEvenNumbers + startNumber;
-               }
+
+      } else if(startNumber % 2){
+
+        sumEvenNumbers = sumEvenNumbers + startNumber;
+      }
     }
-  alert (`Ваш результат: ${sumEvenNumbers}`);
-}else{ 
-  for(startNumber; startNumber<=finalNumber; startNumber++){
-  sumAllNumbers = sumAllNumbers + startNumber;
-     }
-  alert (`Ваш результат: ${sumAllNumbers}`); 
-}
+    alert (`Ваш результат: ${sumEvenNumbers}`);
