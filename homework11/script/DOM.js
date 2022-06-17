e4774ea7436542d064bf589c6e02dceb8f114422
BohdanPanchyshyn <panchyshyn33@gmail.com>
2022-06-17 13:25:31 +0300

    modified HM #1

--- homework11/script/DOM.js
+++ homework11/script/DOM.js
@@ -1,13 +1,20 @@
 alert("Щоб квадрати з'явились на сторінці, необхідно викликати функцію generateBlocks(); або generateBlocksInterval();")
 
-const element = document.getElementById("container");
+let element = document.getElementById("container");
 
 function color() {
    const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0').toUpperCase();
    return randomColor;
 }
 
+
+function clearContainer() {
+   return document.getElementById("container").innerHTML = "";
+}
+
+
 function generateBlocks() {
+   clearContainer();
    const numberSquare = 25;
    for (let i = 0; i < numberSquare; i++) {
       let newSquare = document.createElement('div');
@@ -18,6 +25,7 @@ function generateBlocks() {
 }
 
 function generateBlocksInterval() {
+   clearContainer()
    generateBlocks();
    let oldBlocks = document.getElementsByClassName("article");
    setInterval(() => {
