17574afd0e19af9d9e6a17ccaa9ca07e966674e4
BohdanPanchyshyn <panchyshyn33@gmail.com>
2022-07-19 20:39:09 +0300

    modified HM#14

--- homework14/script/script.js
+++ homework14/script/script.js
@@ -13,53 +13,72 @@ let previosLink = null;
 
 
 async function showActorsFilms() {
-   startLoader();
-   const numberFilms = getNumberFilms.value;
-   if (numberFilms < 1 || numberFilms > 6) alert("Please, enter 1 to 6!"), endLoader();
-   const request = await fetch(`${BASE_URL}/films/${numberFilms}`);
-   const response = await request.json();
-   const massLinkActors = response.characters;
-   getActors(massLinkActors);
+   try {
+      startLoader();
+      const numberFilms = getNumberFilms.value;
+      if (numberFilms < 1 || numberFilms > 6) alert("Please, enter 1 to 6!"), endLoader();
+      const request = await fetch(`${BASE_URL}/films/${numberFilms}`);
+      const response = await request.json();
+      const massLinkActors = response.characters;
+      getActors(massLinkActors);
+   } catch (error) {
+      console.error(error);
+   }
 }
 
-
 async function getActors(params) {
-   const allPromises = params.map((el) => fetch(`${el}`))
-   const responses = await Promise.all(allPromises)
-   const data = await Promise.all(responses.map(r => r.json()))
-   endLoader();
-   createCardActor(data);
+   try {
+      const allPromises = params.map((el) => fetch(`${el}`))
+      const responses = await Promise.all(allPromises)
+      const data = await Promise.all(responses.map(r => r.json()))
+      endLoader();
+      createCardActor(data);
+   } catch (error) {
+      console.error(error);
+   }
 }
 
 async function showPlanets() {
-   startLoader();
-   const request = await fetch(`${BASE_URL}/planets/`);
-   const response = await request.json();
-   const massPlanet = response.results;
-   endLoader();
-   createPlanet(massPlanet);
+   try {
+      startLoader();
+      const request = await fetch(`${BASE_URL}/planets/`);
+      const response = await request.json();
+      const massPlanet = response.results;
+      endLoader();
+      createPlanet(massPlanet);
+   } catch (error) {
+      console.error(error);
+   }
 }
 
 async function nextPlanets() {
-   startLoader();
-   const request = await fetch(`${nextNextLink}`);
-   const response = await request.json();
-   if (response.next) nextNextLink = response.next;
-   if (response.previous) previosLink = response.previous;
-   const massPlanet = response.results;
-   endLoader();
-   createPlanet(massPlanet);
+   try {
+      startLoader();
+      const request = await fetch(`${nextNextLink}`);
+      const response = await request.json();
+      if (response.next) nextNextLink = response.next;
+      if (response.previous) previosLink = response.previous;
+      const massPlanet = response.results;
+      endLoader();
+      createPlanet(massPlanet);
+   } catch (error) {
+      console.error(error);
+   }
 }
 
 async function previosPlanets() {
-   startLoader();
-   const request = await fetch(`${previosLink}`);
-   const response = await request.json();
-   if (response.next) nextNextLink = response.next;
-   if (response.previous) previosLink = response.previous;
-   const massPlanet = response.results;
-   endLoader();
-   createPlanet(massPlanet);
+   try {
+      startLoader();
+      const request = await fetch(`${previosLink}`);
+      const response = await request.json();
+      if (response.next) nextNextLink = response.next;
+      if (response.previous) previosLink = response.previous;
+      const massPlanet = response.results;
+      endLoader();
+      createPlanet(massPlanet);
+   } catch (error) {
+      console.error(error);
+   }
 }
 
 function createCardActor(params) {
@@ -85,7 +104,6 @@ function createPlanet(params) {
       container.append(planet);
    })
 }
-
 
 function addImg(params) {
    let photo = new Image();
